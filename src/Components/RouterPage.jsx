e86cbc25506f359b7098a267508a77a1daedca22
ming9ya <yxmj17@naver.com>
2023-04-15 11:39:06 +0900

    ver02

--- src/Components/RouterPage.jsx
+++ src/Components/RouterPage.jsx
@@ -8,13 +8,14 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link, Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import BookPage from './BookPage';
+import LocalPage from './LocalPage';
 
 const RouterPage = () => {
     return (
         <>
             <Navbar bg="light" expand="lg">
                 <Container fluid>
-                    <Link to ="/book">LOGO</Link>
+                    <Navbar.Brand href="/book">LOGO</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
@@ -23,13 +24,15 @@ const RouterPage = () => {
                             navbarScroll>
                             <Link to="/">Home</Link>
                             <Link to="/book">도서검색</Link>
+                            <Link to="/local">지역검색</Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
             <Switch>
-                <Route path="/" component={HomePage} exact={true}/>
-                <Route path="/book" component={BookPage}/>
+                <Route path="/" component={HomePage} exact={true}> </Route>
+                <Route path="/book" component={BookPage}></Route>
+                <Route path="/local" component={LocalPage}></Route>
             </Switch>
         </>
     )
